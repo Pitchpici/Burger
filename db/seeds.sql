7c8ec84ba6ee5dddbae3c677b7b95fcc82eccbf1
Roxana Milea <roxanamilea@Roxanas-MacBook-Air.local>
2018-03-26 23:55:35 -0400

    almost there

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Veggie 1 Burger");
+INSERT INTO burgers (burger_name) VALUES ("Veggie 2 Burger");
+INSERT INTO burgers (burger_name) VALUES ("Veggie 3 Burger");
