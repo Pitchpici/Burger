2f1c58a055c2fd43b1c81562c393c48211ab846f
Roxana Milea <roxanamilea@Roxanas-MacBook-Air.local>
2018-04-07 22:16:55 -0400

    readme

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL); 
 } else{
   connection = mysql.createConnection({
-  port: 3306,
+  port: 8080,
   host: "127.0.0.1",
   user: "root",
   password: "",
